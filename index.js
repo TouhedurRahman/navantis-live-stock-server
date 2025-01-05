79cbbbf2859fe386856801b0f0aefe4a1b97cd64
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2025-01-05 14:58:32 +0600

    get all depot stock out api added

--- index.js
+++ index.js
@@ -859,6 +859,12 @@ async function run() {
             }
         });
 
+        // get all depot stock-out API
+        app.get('/stock-out-depot', async (req, res) => {
+            const result = await depotStockOutCollections.find().sort({ _id: -1 }).toArray();
+            res.send(result);
+        });
+
         // Send a ping to confirm a successful connection
         await client.db('admin').command({ ping: 1 });
         console.log("Pinged your deployment. You're successfully connected to MongoDB!");
